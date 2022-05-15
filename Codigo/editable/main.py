baae15679342407ae1a1626cc44d0796553be778
EzioFenix <vaitor_victor@hotmail.com>
2022-05-14 21:02:30 -0500

    funciona el reconocimiento de imagenes

--- Codigo/editable/main.py
+++ Codigo/editable/main.py
@@ -1,58 +1,33 @@
-from operator import mod
+from email.mime import image
+from importlib.util import module_from_spec
+from operator import le, mod
 import os
 from pyexpat import model
+from cv2 import line
 import numpy as np # numpy
 import cv2  # biblioteca de open cv
+import time
 
-
-def leer_directorio(isTraining:bool):
-
-    if isTraining:
-        entrenamientoPath=os.path.join("..","entrenamiento")
-        # Leemos los archivos de entrenamiento
-        carpetas=os.listdir(entrenamientoPath)
-        # Leemos cada uno de los modelos de entrenamiento
-        print(carpetas)
-        for subcarpeta in carpetas:
-            ruta=os.path.join(entrenamientoPath,subcarpeta)
-
-            print(ruta)
-            modelo=crear_modelo(ruta)
-            print(modelo.shape)
-
-            # Guardamos el modelo entrenado en un cvs
-            nombre_modelo="{0}en.csv".format(subcarpeta)
-            np.savetxt(nombre_modelo, modelo,fmt='%s', delimiter=",")
-            """ with open(nombre_modelo, "w") as f:
-                #f.write(b'ImageId,Label\n')
-                numpy.savetxt(f, result, fmt='%s', delimiter=",") """
-            
-            # crear imagen
-            # dimensiones y,x
-            imagen=np.zeros((32,32),dtype=np.intc)
-            modelo=list(modelo)
-            for j in range (32):
-                for i in range(32):
-                    imagen[j,i]=modelo.pop(0)
-            
-            # Creamos imagen
-            cv2.imwrite("ne1.jpg",imagen)
-
-
-        
-        
+"""
+Lee una imagen en la dirección que se le de como paramétro y la pasa a un arreglo unidimensional, donde los valores van del 0-255 es decir que estan en blanco y negro
+- Parametro:
+    Ruta de la imagen
+    Ejemplo:
+        "carpeta/imagen1.png"
+- Return:
+    Arreglo unidimensional con valores de 0-255 en escalas de grises, de los pixeles de la imagen
+    Ejemplo:
+        [255,255,255,0,1,...,244]
+"""
 def leer_imagen(nombre_archivo:str)->list[int]:
-    print(nombre_archivo)
-    img = cv2.imread(nombre_archivo,0) # espacio de color BGr, pero con el 0 es gray
-    dimensionY,dimensionX=img.shape
-    dimension=dimensionX * dimensionY
+    #print(nombre_archivo)
     histograma:list[int]=[]
+    dimensionX:int
+    dimensionY:int
 
-    
-
-    # funcion shape
+    img = cv2.imread(nombre_archivo,0) # espacio de color BGr, pero con el 0 es gray´
     #It returns a tuple of the number of rows, columns, and channel
-    # y,x,canal
+    dimensionY,dimensionX=img.shape
 
     # acceder a un pixel
     #px =img[10,15] # y=10,x=15
@@ -60,13 +35,82 @@ def leer_imagen(nombre_archivo:str)->list[int]:
         for i in range(0,dimensionX):
             histograma.append(img[j,i])
 
-    ac=nombre_archivo.split("\\")[-1]
-    np.savetxt("p"+ac + '.csv', histograma,fmt='%s', delimiter=",")
+    # en caso de ver el resultado activar este código
+    """ ac=nombre_archivo.split("\\")[-1]
+    nombre,tipo= ac.split(",")
+    np.savetxt(nombre + '.csv', histograma,fmt='%s', delimiter=",") """
     
     return histograma
 
 
-def crear_modelo(rutaCarpeta:str):
+"""
+Oscurece la imagen de acuerdo al intervalo dado, si el intervalo es 15, aquellos que tengan menor que el intervalo se volverán negro.
+- Por default el valor esta en 30
+"""
+def oscurecer(histograma:list[int],intervalo:int=40)-> list[int]:
+    nuevoHistograma:list[int]=[]
+
+    for px in histograma:
+        if px<intervalo:
+            nuevoHistograma.append(0)
+        elif 255-40<px:
+            nuevoHistograma.append(255)
+        else:
+            nuevoHistograma.append(px)
+    return nuevoHistograma
+
+    
+
+    for elemento in histograma:
+        if  intervalo< elemento:
+            nuevoHistograma.append(255)
+        else:
+            nuevoHistograma.append(0)
+
+    return nuevoHistograma
+
+
+"""
+Regresa el número de la opción seleccionada del menu
+"""
+def menu()->int:
+    opciones=["Salir","Entrenar modelos","Cargar modelos","Clasificar imagen","Realizar test presición"]
+    opcion:str
+    opcionValida=False # Para saber si la opción que eleigío es valida
+
+    while not opcionValida:
+        for i in range(len(opciones)):
+            print('{:<8} {:<20s}'.format(str(i),opciones[i]))
+
+        print("Introduce el número de la opción que deseas ejecutar")
+        # introducir opcion
+        opcion=input("")
+
+        if not opcion.isdigit():
+            print("Tienes que introducir un número, intenta de nuevo")
+            time.sleep(1)
+            os.system("cls")
+        else:
+            opcion:int=int(opcion.strip())
+            if 0<=opcion and opcion<len(opciones):
+                opcionValida=True
+                return opcion
+            else:
+                print("opción no valida,intente de nuevo")
+                time.sleep(1)
+                os.system("cls")
+
+
+"""
+Genera un modelo de un tipo de imagenes
+Parametros:
+    - rutaCarpeta: es la dirección donde se encuentran las imagenes con las cuales se entrenará el modelo
+        Ejemplo: "../entrenamiento/1"
+Return:
+    - Regresa un np.ndarray de una dimensión que contiene los valores en escala de grises del 0 al 255,las dimensiones son x por y=z por ejemplo 32x32=1024 [255,255,0,...,0]
+ 
+"""
+def crear_modelo(rutaCarpeta:str)->np.ndarray:
     carpeta_actual=os.listdir(rutaCarpeta)
     #print(carpeta_actual)
     numImagenes=len(carpeta_actual)
@@ -94,9 +138,223 @@ def crear_modelo(rutaCarpeta:str):
     return histogramaPromedio
 
 
+"""
+Genera los modelos entrenados en la carpeta de entrenados
+Return:
+    - lista con las direcciones de los archivos generados
+    Ejemplo: ["../entrenado/img1.png","../entrenado/img2.png"]
+"""
+def generarModelos()->list[str]:
+    direccionModelos:list[str]=[]
 
-def main():
+    entrenamientoPath=os.path.join("..","entrenamiento")
+    # Leemos los archivos de entrenamiento
+    carpetas=os.listdir(entrenamientoPath)
+    # Leemos cada uno de los modelos de entrenamiento
+    print(carpetas)
+    for subcarpeta in carpetas:
+        ruta=os.path.join(entrenamientoPath,subcarpeta)
+
+        print(ruta)
+        modelo=crear_modelo(ruta)
+        print(modelo.shape)
+
+        """# Guardamos el modelo entrenado en un cvs
+        nombre_modelo="{0}en.csv".format(subcarpeta)
+        np.savetxt(nombre_modelo, modelo,fmt='%s', delimiter=",")
+            with open(nombre_modelo, "w") as f:
+            #f.write(b'ImageId,Label\n')
+            numpy.savetxt(f, result, fmt='%s', delimiter=",") """
+        
+        # crear imagen
+        # dimensiones y,x
+        imagen=np.zeros((32,32),dtype=np.intc)
+        modelo=list(modelo)
+        for j in range (32):
+            for i in range(32):
+                imagen[j,i]=modelo.pop(0)
+        
+        # Creamos imagen
+        nombreNuevo=os.path.join("..","entrenado",subcarpeta+".jpg")
+        print("pp")
+        print(nombreNuevo)
+        cv2.imwrite(nombreNuevo,imagen)
+
+        #agregamos a la lista de archivos
+        direccionModelos.append(nombreNuevo)
+    return direccionModelos
+
+
+def cargarImagen()->str:
+    rutaInput=os.path.join("..","input")
+    archivos=os.listdir(rutaInput)
+    opcion:str
+    opcionValida=False # Para saber si la opción que eleigío es valida
+
+    # mostar imagenes
+    while not opcionValida:
+        for i in range(len(archivos)):
+            print('{:<8} {:<20s}'.format(str(i),archivos[i]))
+
+        print("Introduce el número de la imagen")
+        # introducir opcion
+        opcion=input("")
+
+        if not opcion.isdigit():
+            print("Tienes que introducir un número, intenta de nuevo")
+            time.sleep(1)
+            os.system("cls")
+        else:
+            opcion:int=int(opcion.strip())
+            if 0<=opcion and opcion<len(archivos):
+                opcionValida=True
+                return os.path.join(rutaInput,archivos[opcion])
+            else:
+                print("opción no valida,intente de nuevo")
+                time.sleep(1)
+                os.system("cls")
+    
+
+def cargarModelos()->list[str]:
+    rutaEntrenados=os.path.join("..","entrenado")
+    archivos=os.listdir(rutaEntrenados)
+    archivosValidos:list[str]=[] # Archivos que si son .jpg
+
+    # Agregamos las rutas de los archivos de modelo validos en la carpeta de entrenamiento
+    for archivo in archivos:
+        if archivo.endswith(".jpg"):
+            ruta=os.path.join(rutaEntrenados,archivo)
+            archivosValidos.append(ruta)
+    return archivosValidos
+    
+"""
+Calcula la probabilidad a posterior de los pixeles de una imagen
+Parametros:
+    - Modelo: es la imagen de modelo, en este caso esta leido de una imagen con openCv en una un arreglo de 1 dimensión
+    - Imagen: es un imagen leida con openCv en arreglo de 1 dimensión
+"""
+def obtenerProbabilidadPosteri(modelo:list[int],imagen:list[int]):
+    resta:list[int]=[]
+    probabilidad:float=0.0
+    for i in range(len(modelo)):
+        resta.append(abs(modelo[i]-imagen[i]))
+
+    for i in resta:
+        probabilidad+=1 -i/255
+
+    return probabilidad /len(modelo)
+
+
+
+def clasificarImagen(direccionImagen:str,direccionesModelos:list[str])->None:
+    modeloActual:list[int]
+
+    redimensionar(direccionImagen)
+    imagenActual:list[int]=leer_imagen(direccionImagen)
+    probabilidadMayor=0.0
+    probababilidadesModelos=[int]
+
+    probabilidadApriori:float=1/len(direccionesModelos)
+
+
+    for direccion in direccionesModelos:
+        print(direccionesModelos)
+        modeloActual=leer_imagen(direccion)
+        modeloActual=oscurecer(modeloActual)
+        probabilidadActual=obtenerProbabilidadPosteri(modeloActual,imagenActual) 
+        probababilidadesModelos.append(probabilidadActual)
+
+        if probabilidadMayor<=probabilidadActual:
+            probabilidadMayor=probabilidadActual
+
+    # Mostrar resultado
+    indiceResultado=probababilidadesModelos.index(probabilidadMayor)
+    nombreResultado=direccionesModelos[indiceResultado-1]
+    print(f"Tu imagen es: {nombreResultado}")
+    print(f"La probabilidad es: {probabilidadMayor *probabilidadApriori}")
+    print(f"La probabilidad de coincidencia es :{probabilidadMayor}")
+        
+
+def redimensionar(direccionImagen:str)->None:
+    img=cv2.imread(direccionImagen)
+    dimensionX,dimensiony,ne= img.shape
+    if not(dimensionX==32 and dimensiony==32):
+        res= cv2.resize(img, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(direccionImagen,res)
+        print("cambio tamaño completado ...")
+
+
+
+def testPrecision(direccionImagen:str)->None:
     pass
 
+def main():
+    # variables globales 
+    modelosCargados:list[str]=cargarModelos()
+    isModelosCargados:bool = 0<len(modelosCargados)# saber si los modelos estan cargados
+    imagenCargada:list
+    isImagenCargada:bool=True
+    direccionImagenCargada:str="1.png"
+    opcion=1
+
+    while opcion!=0:
+        opcion=menu()
+        if opcion==0:
+            print("Hasta luego")
+            time.sleep(1)
+        elif opcion==1:
+            print("Entrenanando modelos")
+            if isModelosCargados:
+                print("Deseas volver a generar los modelos")
+                respuesta=input("s=si,n=no")
+                respuesta=respuesta.strip()
+                if respuesta=="s":
+                    modelosCargados=generarModelos()
+                    print(modelosCargados)
+                    isModelosCargados=True
+                elif respuesta=="n":
+                    print("esta bien")
+                    time.sleep(1)
+                else:
+                    print("tu respuesta es incorrecta, vuelve a intentar")
+                    time.sleep(1)
+            else:
+                modelosCargados=generarModelos()
+        elif opcion==2:
+            print("Cargar Modelos")
+            dirrecciones=cargarModelos()
+            if len(dirrecciones)==0:
+                print("No habia modelos")
+            else:
+                print("Se cargarón perfectamente los modelos")
+                modelosCargados=dirrecciones
+                isModelosCargados=True 
+        elif opcion==3:
+            print("Clasificar una imagen")
+            if isModelosCargados:
+                if isImagenCargada:
+                    respuesta=input("Desea cargar otra imagen")
+                    respuesta=respuesta.strip()
+                    if respuesta=="s":
+                        isImagenCargada=False
+                    else:
+                        print(f"imagen :{direccionImagenCargada}")
+                        print(modelosCargados)
+                        clasificarImagen(direccionImagenCargada,modelosCargados)
+                if not isImagenCargada:
+                    direccionImagenCargada=cargarImagen()
+                    print(f"direccion: {direccionImagenCargada}")
+                    isImagenCargada=True
+                    clasificarImagen(direccionImagenCargada,modelosCargados)
+        elif opcion==4:
+            print("Realizar test precisión")
+            if isImagenCargada:
+                testPrecision(direccionImagenCargada)
+            else:
+                print("primero necesitas clasificar la imagen para poder hacer test de precesión")
+
 if __name__ == "__main__":
-    leer_directorio(True)
+    #leer_directorio(True)
+    # menu()
+    main()
+    #^
--- README.md
+++ README.md
@@ -59,6 +59,12 @@ En el caso de este proyecto se ha delimitado a que las imágenes de entrenamient
 
 Recordando que en este caso que dado un vector de condiciones $\vec{Q}$ que contiene los valores $[q_1,q_2,...,q_j]$ para $j$ condiciones, a los cuales debe igualarse $Am_i$, obtener $Y_{max}(\vec{Q})$ que es la probabilidad más grande para dicho vector.
 
+## Teoría
+
+
+
+
+
 # Solución
 
 ## Teoría
@@ -67,30 +73,135 @@ Recordando que en este caso que dado un vector de condiciones $\vec{Q}$ que cont
 
 En este caso son 10 conjuntos de imágenes, cada uno con 15 imágenes como se aprecia en este caso para $C_1=1$
 
-![image-20220506183750690](img/README/image-20220506183750690.png) 
+![data-set número 1](img/README/image-20220506183750690.png) 
 
 ### Segundo paso,  elaboración de historiagrama
 
 Para este método de predicción tenemos que que tomar $32 \hspace{0.3cm}x \hspace{0.3cm} 32=1024$ .Será un histograma que contiene el número que que tan negro es, si este es 0 significa que lo es, sin embargo si es 255 es blanco, si es un intermedio entre estos es un gris. Sólo tenemos esos dos, ya que limitamos nuestra entrada a dichos dos colores, para simplificar, si una entrada fuera de otro color tendríamos que cambiarla a grises, ya que los que nos interesa en esta clasificación es la forma, no el color.
 
-Para la creación del histograma, el  programa lo hace en automático como se ve en la siguiente ejecución 
+primero definamos los valores de las imágenes para un modelo, suponga la existencia de un  conjunto de imagenes de comparten las siguientes características **{escala de grises, están escritos a mano, tienen la mismas dimensiones, esta escrito el mismo número}**.
+
+Veamos un ejemplo para las características anteriormente dadas para el modelo del número `1` definamos las características:
+
+{escala de gristes,escritos a mano,tienen dimensión 32x32,el número es 1 y son 15 elementos imagenes}
+
+Con el anterior conjunto podemos obtener que $n=15$ $x=32,y=32$
+
+Definamos que una imagen es lo siguiente
+$$
+I=
+\begin{equation}
+\begin{pmatrix}
+p[0,0] & p[0,1] & \cdots & p[0,x]\\
+p[1,0] & p[1,1] & \cdots & p[1,x]\\
+\vdots & \vdots & \ddots & \vdots\\
+p[y,0] & p[y,0] & \cdots & p[y,x]
+\end{pmatrix}
+\end{equation}
+$$
+Como vamos a trabajar con varias imágenes es necesario definir una pequeña notacion $p1[0,0]$ este es el pixel [0,0] para la **imagen 1**, $pn[0,0]$ es el pixel [0,0] de la `n-esima` imágen.
+
+También definiré una matriz  llama $M=media$ , en este caso tiene un subíndice $1$ eso significa que es la matriz media de imagenes 1
+$$
+M_1= \begin{equation}
+\begin{pmatrix}
+pm[0,0] & pm[0,1] & \cdots & pm[0,x]\\
+pm[1,0] & pm[1,1] & \cdots & pm[1,x]\\
+\vdots & \vdots & \ddots & \vdots\\
+pm[y,0] & pm[y,0] & \cdots & pm[y,x]
+\end{pmatrix}
+\end{equation}
+$$
+para calcular  cada uno de los puntos de la matriz $M$ usamos lo siguiente
+$$
+pm[0,0]= \frac{p1[0,0]+ p2[0,0] + p3[0,0]+ \cdots + pn[0,0]}{n}
+$$
+Es la media aritmética para el pixel 1  de las imágenes de número 1, se tiene que realizar lo mismo para todos los los pixeles de la matriz $M$, pero terminar todos los puntos de la matriz $M$ sería el modelo sólo para la imágenes con el número 1 pero para ser un clasificador tiene que tener más que de sólo un sólo resultado, y para este proyecto tiene que clasificar entre los posibles resultados de $[0,1,2,3,4,\cdots,9]$ y por tanto tendríamos los modelos  $[M_0,M_1,M_2,M_3,M_4,\cdots,M_9]$.
+
+### Tercer paso tratar la imagen
+
+La entrada no es siempre la que queremos y por ello es necesario manipularla de tal manera que la entrada  coincida con una entrada  que sea posible tratarla.
+
+Para resolver el problema, vamos a usar el teorema de Bayes con la siguiente ecuación:
+$$
+P(Y=y_i|X=x_o)= \frac{P(X=x_0| Y=y_i)\cdot P(Y=y_i)}{P(X=x_o)}
+$$
+Con dicha ecuación empleándola sobre una imagen podemos saber  cuando es la probabilidad por cada pixel de la imagen que queremos clasificar
+$$
+P(Y=y_i) \hspace{1cm} \text{Probabilidad a priori}
+$$
+
+$$
+P(X=x_0| Y=y_i) \hspace{1cm} \text{Probabilidad a posterior}
+$$
+
+$$
+P(X=x_o) \hspace{1cm} \text{Termino de normalización}
+$$
+
+$$
+P(Y=y_i|X=x_o)  \hspace{1cm} \text{Probabilidad  de que un pixel sea un modelo}
+$$
+
+Vamos desglosando cada uno  en el orden anterior
+
+#### Probabilidad a priori
+
+Definamos las variables $yi \in \{0,1,2,\cdots,9\}$  ya que son las posibles categorías que hay , es decir que es la **probabilidad de que sea alguna de las categorías** y por tanto se calcula de la siguiente manera
+$$
+P(Y=y_i)=\frac{1}{n}=\frac{1}{10}\%
+$$
+recuerda que  $n$ es el número de modelos.
+
+Hay que tener en consideración que esto es debido a que todos los modelos se entrenaron con el mismo número de imágenes, de no ser así la formula  sería :
+$$
+P(Y=y_i)=\frac{\text{Num imagenes modelo }y_i}{\text{Num imagenes totales}}
+$$
+Si para entrenar cada modelo usaste 15 imágenes y son 10 modelos entonces tendrías $150 $ imagenes totales.
+
+Como para el modelo `Num imagenes modelo`  $y_i$ fueron 15 imágenes para entrenar a este modelo, por tanto 
+$$
+\text{Num imagenes modelo }y_i=15
+$$
+Entonces la probabilidad para que el modelo sea igual a 1 $P(Y=1)$ :
+$$
+P(Y=1)=\frac{15}{150}\%
+$$
+
+#### Probabilidad a posterior
+
+Es una probabilidad condicional y por ello recordar la fórmula
+$$
+P(B|A)=\frac{P(A\cap B)}{P(A)}
+$$
+Entonces  utilizamos la anterior formula 
+$$
+P(X=x_0| Y=y_i)=\frac{P(X=x_o\cap Y=y_i)}{P(X=x_o)}
+$$
+Esta probabilidad te dice **que tan probable es que dado un modelo, el pixel ** $X=x_o$ sea del valor esperado. 
+$$
+P(X=x_o)
+$$
+
+
+Te dice de **cual es la probabilidad de que el pixel tenga el valor dado**, por ejemplo 
+
+### Cuarto paso  obtener la probabilidad de una imagen 
+
+#### 
+
+Para la solución requerimos saber de dos cosas:
+
+- Teorema de Bayes para implementar la probabilidad.
+- Una imagen que no pertenezca al data-set con el cual entrenamos los modelos.
+
+Pasamos la imagen **input** que queremos saber a que clasificación pertenece, llamaremos a la matriz $I$.
+
+**C*
 
 
 
-Cabe aclarar que el programa en automático puede entrenarse para otro conjunto $C$ , lo único que necesita es llamar a las carpetas como el nombre de los elementos $c_n$, véase que en el caso de el conjunto de las pru
-
-
-
-,	
-
-## 
-
-
-
-Para la solución requerimos de dos cosas:
-
-- Teorema de Bayes implementado
-- Un data-set de 
+#### Quinto paso  obtener tiempo de ejecución y precisión
 
 ## Pseudocódigo
 
